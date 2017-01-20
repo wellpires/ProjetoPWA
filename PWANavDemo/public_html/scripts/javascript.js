ba247c07857663ccfa5746395f1e45dafd3d1e9d
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-01-20 18:38:09 -0200

    Adicionando os ponto e virgular faltantes

--- PWANavDemo/public_html/scripts/javascript.js
+++ PWANavDemo/public_html/scripts/javascript.js
@@ -4,20 +4,20 @@
  * and open the template in the editor.
  */
 
-window.onload = function(){
-    document.getElementById("menuButton").onclick = function(){
-        if(window.innerWidth > 600){
+window.onload = function () {
+    document.getElementById("menuButton").onclick = function () {
+        if (window.innerWidth > 600) {
             document.getElementById("mySidenav").style.width = "250px";
-            document.getElementById("content").style.marginLeft = "250px"
+            document.getElementById("content").style.marginLeft = "250px";
         } else {
             document.getElementById("mySidenav").style.width = "250px";
         }
-        
+
     };
-    document.getElementById("closeButton").onclick = function(){
+    document.getElementById("closeButton").onclick = function () {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("content").style.marginLeft = "0";
-    }
+    };
 };
 
 
