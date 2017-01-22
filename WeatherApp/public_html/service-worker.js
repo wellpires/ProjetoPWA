497e90cefa23cb15f5b9d86fb1569004a6983b69
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-01-22 21:47:44 -0200

    WeatherApp Corrigindo o service workers para funcionar da forma desejada

--- WeatherApp/public_html/service-worker.js
+++ WeatherApp/public_html/service-worker.js
@@ -23,7 +23,22 @@ var filesToCache = [
     '/images/sleet.png',
     '/images/snow.png',
     '/images/thunderstorm.png',
-    '/images/wind.png'];
+    '/images/wind.png',
+    '/node_modules/lovefield/dist/error_code.json',
+    '/node_modules/lovefield/dist/lovefield.d.ts',
+    '/node_modules/lovefield/dist/lovefield.es6.js',
+    '/node_modules/lovefield/dist/lovefield.es6.js.map',
+    '/node_modules/lovefield/dist/lovefield.externs.js',
+    '/node_modules/lovefield/dist/lovefield.js',
+    'node_modules/lovefield/dist/lovefield.min.js',
+    '/node_modules/lovefield/dist/lovefield.min.js.map',
+    '/node_modules/lovefield/spac/template/database.jstemplate',
+    '/node_modules/lovefield/spac/template/schema.jstemplate',
+    '/node_modules/lovefield/spac/template/templates.json',
+    '/node_modules/lovefield/spac/codegen.js',
+    '/node_modules/lovefield/spac/lovefield-spac',
+    '/node_modules/lovefield/spac/parser.js',
+    '/node_modules/lovefield/spac/spac.js'];
 
 self.addEventListener('install', function (e) {
     e.waitUntil(caches.open(cacheName).then(function (cache) {
@@ -31,19 +46,19 @@ self.addEventListener('install', function (e) {
     }));
 });
 
-self.addEventListener('activate', function(e){
-    e.waitUntil(caches.keys().then(function(keyList){
-        return Promise.all(keyList.map(function(key){
-            if(key !== cacheName && key !== dataCacheName){
+self.addEventListener('activate', function (e) {
+    e.waitUntil(caches.keys().then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+            if (key !== cacheName && key !== dataCacheName) {
                 return caches.delete(key);
             }
         }));
     }));
 });
 
-self.addEventListener('fetch', function(e){
+self.addEventListener('fetch', function (e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
-    e.respondWith(caches.match(e.request).then(function(response){
+    e.respondWith(caches.match(e.request).then(function (response) {
         return response || fetch(e.request);
     }));
 });
