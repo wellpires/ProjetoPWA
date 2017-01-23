1a680dd92e3c3605f7a605a648ef1c32cd733a54
Wellington Gonçalves Pires <wellington.goncalves@live.com>
2017-01-23 17:55:20 -0200

    PWANavDemo Commit antes de criar nova branch Adição do arquivo service-worker e alteração do arquivo javascript.js

--- PWANavDemo/public_html/scripts/javascript.js
+++ PWANavDemo/public_html/scripts/javascript.js
@@ -4,12 +4,20 @@
  * and open the template in the editor.
  */
 window.onload = function () {
-    
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+            console.log('Service Worker registered ', registration);
+        }).then(function (e) {
+            console.log('ERRO ', e);
+        });
+    }
+
     var menuButton = document.getElementById("menuButton");
     var mySidenav = document.getElementById("mySidenav");
     var content = document.getElementById("content");
     var closeButton = document.getElementById("closeButton");
-    
+
     menuButton.onclick = function () {
         if (window.innerWidth > 600) {
             mySidenav.style.width = "250px";
@@ -20,8 +28,7 @@ window.onload = function () {
 
     };
     closeButton.onclick = function () {
-        mySidenav.style.width = "0";
-        content.style.marginLeft = "0";
+        esconderMenu();
     };
 
     $('#divCentral').load('main.html');
@@ -30,12 +37,16 @@ window.onload = function () {
     alterarConteudoCentral('linkHome', 'main.html');
     alterarConteudoCentral('linkAbout', 'about.html');
     alterarConteudoCentral('linkServices', 'services.html');
-    alterarConteudoCentral('linkContact', 'contact.html');
-
 };
 
 function alterarConteudoCentral(link, page) {
     document.getElementById(link).addEventListener('click', function () {
         $('#divCentral').load(page);
+        esconderMenu();
     });
+}
+
+function esconderMenu() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
 }
